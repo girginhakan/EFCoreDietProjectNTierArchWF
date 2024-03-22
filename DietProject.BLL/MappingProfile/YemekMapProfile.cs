eb6a5d028f81a2517238cc9506c70708ba41dea7
benguurgen <benguurgen@gmail.com>
2024-03-22 21:45:54 +0300

    .

--- DietProject.BLL/MappingProfile/YemekMapProfile.cs
+++ DietProject.BLL/MappingProfile/YemekMapProfile.cs
@@ -17,7 +17,7 @@ namespace DietProject.BLL.MappingProfile
             ForMember(y => y.Yemekler, b => b.MapFrom(src => src.Yemekler)).ReverseMap();
 
             CreateMap<KullaniciOgunYemekPorsiyonModel, KullaniciOgunYemekPorsiyon>().
-                ForMember(y => y.Yemek, k => k.MapFrom(src => src.Yemek)).ReverseMap(); //hoca reversemap olmadan yazmıstı eklemesini yaptık, ogun, kullanıcı, porsiyondada bu sekilde yazdık.
+                ForMember(y => y.Yemek, k => k.MapFrom(src => src.Yemek)).ReverseMap(); //hoca reversemap olmadan yazmıstı eklemesini yaptık, ogun, kullanıcı, porsiyondada bu sekilde yazdık....
 
             //CreateMap<KullaniciOgunYemekPorsiyonModel, KullaniciOgunYemekPorsiyon>().
             // ForMember(y => y.Ogun, k => k.MapFrom(src => src.OgunModel));
